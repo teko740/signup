6062cfa564d6e0056cf6874209bb136f5cd62932
Terrence Eko <81269899+teko740@users.noreply.github.com>
2021-04-16 23:37:31 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const firstName = form['firstname'].value;
+    const lastName = form['lastname'].value;
+    const email = form['email'].value;
+    const password = form['password'].value;
+
+    if (firstName === '') {
+        addErrorTo('firstname', 'First Name is required');
+    } else {
+        removeErrorFrom('firstname');
+    }
+
+    if (lastName === '') {
+        addErrorTo('lastname', 'Last Name is required');
+    } else {
+        removeErrorFrom('lastname');
+    }
+
+    if (email === '') {
+        addErrorTo('email', 'Email is required');
+    } else if (isValid(email)) {
+        addErrorTo('email', 'Email is not valid');
+    } else {
+        removeErrorFrom('email');
+    }
+
+    if (password === '') {
+        addErrorTo('password', 'Password is required');
+    } else {
+        removeErrorFrom('password');
+    }
+});
+
+function addErrorTo(field, message) {
+    const formControl = form[field].parentNode;
+    formControl.classList.add('error');
+
+    const small = form[field].parentNode.querySelector('small');
+    small.innerText = message;
+    small.style.opacity = '1';   
+}
+
+function removeErrorFrom(field) {
+    const formControl = form[field].parentNode;
+    formControl.classList.remove('error');  
+
+    const small = formControl.parentNode.querySelector('small');
+    small.style.opacity = '0';  
+}
+
+function isValid(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
